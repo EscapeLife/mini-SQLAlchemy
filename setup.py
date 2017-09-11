f74704a32d05367a1a65058207d454afc587c849
Escape <240901017@qq.com>
2017-09-11 11:33:23 +0800

    modified setup.py at rename

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(name='m',
-      version='0.1.0',
-      packages=['mini-orm', 'mini-orm.security', 'mini-orm.extensions', 'mini-orm.extensions.sqlalchemy'],
-      install_requires=[
-          'WebOb>=1.6.1',
-          'sqlalchemy>=1.0.0',
-          'pyhocon>=0.3.0',
-      ],
-      author="Escape",
-      author_email="wenpanhappy@126.com",
-      description="This is a very mini SQLAlchemy",
-      license="Apache-2",
+        version='0.1.0',
+        packages=['mini-SQLAlchemy', 'mini-SQLAlchemy.security', 'mini-SQLAlchemy.extensions', 'mini-SQLAlchemy.extensions.sqlalchemy'],
+        install_requires=[
+            'WebOb>=1.6.1',
+            'sqlalchemy>=1.0.0',
+            'pyhocon>=0.3.0',
+        ],
+        author="Escape",
+        author_email="wenpanhappy@126.com",
+        description="This is a very mini SQLAlchemy",
+        license="Apache-2",
       )
